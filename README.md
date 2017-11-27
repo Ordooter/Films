1d8d0b21c8b882c3908f334104bea44db52debdf
Ordooter <ordooter@gmail.com>
2017-11-27 19:57:40 +0100

    Final Project

---
+++ README.md
@@ -0,0 +1,1 @@
+Codeline Assignment: Films App Using Laravel and AngularJs
---
+++ src/app/constant.ts
@@ -0,0 +1,7 @@
+// Use the @Injectable decorator for your Constants class
+
+//you can replace Films with your-root-folder-name 
+
+'use strict';
+export const api_url: string ='/opt/lampp/htdocs/Films/server/public/';
+export const image_url: string ='/opt/lampp/htdocs/Films/server/public/';
